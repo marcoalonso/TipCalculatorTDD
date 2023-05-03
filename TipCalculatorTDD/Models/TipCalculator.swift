fe917f042c58bb8cf118b3cf1edb37cb356ad602
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-02 20:32:10 -0600

    test_should_calculate_tip_succesfully

---
+++ TipCalculatorTDD/Models/TipCalculator.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculator.swift
+//  TipCalculatorTDD
+//
+//  Created by Marco Alonso Rodriguez on 02/05/23.
+//
+
+import Foundation
+
+
+class TipCalculator {
+    
+    
+    func calculate(total: Double, tipPercentage: Double) -> Double {
+        return total * tipPercentage
+    }
+}
