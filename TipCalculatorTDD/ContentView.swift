542b443abbd80a5de4209af229b1380b3ac1a51f
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-02 20:20:40 -0600

    Unit test bundle added

--- TipCalculatorTDD/ContentView.swift
+++ TipCalculatorTDD/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var total: String = ""
+    @State private var tipPercentage: Double = 0.2
+    @State private var tip: String?
+    @State private var message: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            
+            VStack {
+                
+                TextField("Enter total", text: $total)
+                    .textFieldStyle(.roundedBorder)
+                
+                Picker(selection: $tipPercentage) {
+                    Text("10%").tag(0.1)
+                    Text("20%").tag(0.2)
+                    Text("30%").tag(0.3)
+                } label: {
+                    EmptyView()
+                }.pickerStyle(.segmented)
+                
+                
+                Button {
+                    
+                } label: {
+                    Text("Calculate")
+                        .padding(5)
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .background(Color.gray)
+                        .cornerRadius(16)
+                        .padding(10)
+                }
+
+                Text(message)
+                    .padding(.top, 50)
+                
+                Spacer()
+                
+                Text(tip ?? "")
+                .font(.system(size: 54))
+                
+                Image("calculator")
+                    .resizable()
+                
+                Spacer()
+                .navigationTitle("Tip Calculator")
+            }.padding()
+            
         }
-        .padding()
     }
 }
 
