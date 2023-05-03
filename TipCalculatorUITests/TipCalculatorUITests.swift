002c96896563c9686ef06f113f29a0f5b3d5f7cf
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-03 08:53:59 -0600

    18 Refactoring tests

--- TipCalculatorUITests/TipCalculatorUITests.swift
+++ TipCalculatorUITests/TipCalculatorUITests.swift
@@ -8,23 +8,23 @@
 import XCTest
 
  class when_content_view_is_shown: XCTestCase {
+     
+     private var app: XCUIApplication!
+     
+     override func setUp() {
+         app = XCUIApplication()
+         continueAfterFailure = false
+         app.launch()
+     }
 
      func test_should_make_sure_that_the_total_text_field_contains_default_value(){
          
-         let app = XCUIApplication()
-         continueAfterFailure = false
-         app.launch()
-         
          let totalTextField = app.textFields["totalTextField"]
-         
          XCTAssertEqual(totalTextField.value as! String, "Enter total")
      }
      
      func test_should_make_sure_the_15_percent_default_tip_option_is_selected() {
-         let app = XCUIApplication()
-         continueAfterFailure = false
-         app.launch()
-         
+
          let tipPercentageSegmentedControl = app.segmentedControls["tipPercentageSegmentedControl"]
          let segmentedControlButton = tipPercentageSegmentedControl.buttons.element(boundBy: 2)
          
@@ -33,4 +33,8 @@ import XCTest
          XCTAssertTrue(segmentedControlButton.isEnabled)
      }
    
+     ///*- After each test run *
+     override func tearDown() {
+         //Clean up
+     }
 }
