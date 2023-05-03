06b2c4e039d5ed5a493195d3f097d334d6cf891a
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-02 18:19:36 -0600

    Getting Started

---
+++ TipCalculatorTDD/TipCalculatorTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorTDDApp.swift
+//  TipCalculatorTDD
+//
+//  Created by Marco Alonso Rodriguez on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
